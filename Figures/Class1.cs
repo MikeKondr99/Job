1d1e6947851bf4d7de5c13ffa2a7d87f06e9ca94
MikeKondr99@gmail.com <MikeKondr99@gmail.com>
2022-07-22 21:47:51 +0300

    Add project files.

---
+++ Figures/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace Figures
+{
+    public class Class1
+    {
+
+    }
+
+    public class Circle : HasArea
+    {
+        public double Raduis { get; set; }
+        public double Area => throw new NotImplementedException();
+    }
+
+    interface HasArea
+    {
+        double Area { get; }
+    }
+}
---
+++ FiguresTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FiguresTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
